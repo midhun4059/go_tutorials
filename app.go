86fd96d01eb6ea7e213fc01fcdbda8874ed54f26
midhun4059 <midhunrpillai4059@gmail.com>
2025-02-16 17:05:45 +0530

    Displaying sql data in terminal

--- app.go
+++ app.go
@@ -1,18 +1,53 @@
 package main
 
-import(
-
+import (
+	"database/sql"
 	"fmt"
-	"net/http"
-	
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func firstFunc(w http.ResponseWriter,r *http.Request){
-	fmt.Fprint(w,"First message displayed on website")
+func main() {
+	
+	// create a database object which can be used
+	// to connect with database.
+	db, err := sql.Open("mysql", "root:1234@tcp(0.0.0.0:3306)/customer")
+	
+if err !=nil{
+	panic(err)
 }
 
-func main(){
-	http.HandleFunc("/",firstFunc)
-	fmt.Print("Server is running on 3000")
-	http.ListenAndServe(":3000",nil)
-}	
+err = db.Ping()
+
+if err!=nil{
+	panic(err)
+}
+
+result,err:=db.Query("select * from users")
+
+if err !=nil{
+	panic(err)	
+}
+
+for result.Next(){
+
+	
+	var id int 
+	var name string
+	var age  int
+
+	result.Scan(&id,&name,&age)
+
+	if err !=nil{
+		panic(err)
+	}
+
+
+	
+
+	fmt.Printf("id: %d ,name: %s,age: %d \n",id,name,age )
+	
+	
+}
+fmt.Print(" data is sucessfully displayed on terminal")
+
+}
